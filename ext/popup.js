d18ed633e16f2cd02c92eda5d277a7cb9e62bff6
Khan <khan.ahmed@city.ac.uk>
2017-07-11 00:00:36 +0100

    Need to understand how data is stored using chrome.storage.sync.set, seems to reduce everything to only key:values. Gets rid of the Proxy and Add function. Model cannot be created from the Object recieved from chrome.storage.sync.get.

--- ext/popup.js
+++ ext/popup.js
@@ -1,39 +1,75 @@
+// https://stackoverflow.com/questions/2068344/how-do-i-get-a-youtube-video-thumbnail-from-the-youtube-api
+// https://stackoverflow.com/questions/15005500/loading-cross-domain-endpoint-with-jquery-ajax
+// https://gist.github.com/FLamparski/1122e08edeef19ff0913
+/*
+	{
+		<ID> : { title : "title" },
+		<ID> : { title : "title" }
+	}
+*/
+
+_model = {};
+_handler = {
+	set : function(target, key, value){
+		// Observe when array is being appended to
+		target[key] = value;
+		console.log(`Key ${key} to Value ${value}`)
+		return true;
+		// update DOM
+	}
+}
+
+function Model(target, handler){
+	target.add = (id, title) => { target[id] = {"title" : title} };
+	return new Proxy(target, handler);
+}
+
+model = new Model(_model, _handler);
+
+// model = new function(){
+// 	let r = new Proxy(target, handler);
+// 	r.add = id =>{ model[id] = {"title" : title} };
+// 	return r;
+// }
+
 document.addEventListener("DOMContentLoaded", function(){
+	chrome.tabs.query({active : true,currentWindow : true },
 
-	chrome.tabs.query({
-		active : true,
-		currentWindow : true
-	}, function(tabs) {
+		function(tabs) {
 
-		url = tabs[0].url;
+			url = tabs[0].url;
+			if (url.match(/youtube/) == null) return;
+			id = url.match(/[^=]*$/)[0];
+			title = tabs[0].title;
 
-		chrome.storage.sync.get("urls", function(data){
+			chrome.storage.sync.get("model", function(data){
 
-			list = data["urls"];
+				// If this is the first time saving to storage
+				// then `data["model"]` will return an empty
+				// object. In this case `model` will still
+				// refer to the data strcuture previously
+				// defined.
+				//
+				// This is a really weird pattern huh ._.
+				console.log(data.model);
 
-			try{
+				//                                                vvvvvvvvvvvvvvvvvvvvvvvvv
+				// Is not recieving the right type of objects
+				// to create a Model object with.
+				// TO FIX
+				if (!jQuery.isEmptyObject(data["model"])) model = new Model(data["model"]);
 
-				list.push(url);
+				model.add(id, title);
 
-			} catch (err) {
+				chrome.storage.sync.set({"model" : model});
+				
+				// update dom
+				for (var key in model){
+					console.log(`Key ${key}`);
+					console.log(`Title value model[${key}].title`);
+				}
 
-				list = [];
-				list.push(url);
-
-			}
-
-			list = jQuery.uniqueSort(list);
-
-			chrome.storage.sync.set({"urls" : list});
-
-			for ( u in list ){
-				$("#list").append(
-					$("<li>").text(list[u])
-				);
-			}
-			
 		});
 
 	});
-
 });
